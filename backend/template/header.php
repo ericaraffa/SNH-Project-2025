f4f726e9263f7635ec820978c1985dc47a60aa4b
Riccardo Fiorini <rickyfiorini@yahoo.it>
2025-02-11 12:47:46 +0100

    Added privilege check

--- backend/template/header.php
+++ backend/template/header.php
@@ -37,18 +37,12 @@
                             <li>
                                 <a href="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ext-white md:over:text-blue-500 over:bg-gray-700 over:text-blue-500 md:over:bg-transparent order-gray-700" aria-current="page">Home</a>
                             </li>
-                            <li>
-                                <a href="/novel.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ext-white md:over:text-blue-500 over:bg-gray-700 over:text-blue-500 md:over:bg-transparent order-gray-700">Novels</a>
-                            </li>
                             <?php if ($user != null) { ?>
                                 <li>
-                                    <a href="/bookshelf.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ext-white md:over:text-blue-500 over:bg-gray-700 over:text-blue-500 md:over:bg-transparent order-gray-700">Bookshelf</a>
+                                    <a href="/novel.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ext-white md:over:text-blue-500 over:bg-gray-700 over:text-blue-500 md:over:bg-transparent order-gray-700">Novels</a>
                                 </li>
                                 <li>
                                     <a href="/profile.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ext-white md:over:text-blue-500 over:bg-gray-700 over:text-blue-500 md:over:bg-transparent order-gray-700">Profile</a>
-                                </li>
-                                <li>
-                                    <a href="/orders.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ext-white md:over:text-blue-500 over:bg-gray-700 over:text-blue-500 md:over:bg-transparent order-gray-700">Orders</a>
                                 </li>
                                 <li>
                                     <form action="/logout.php" method="POST">
@@ -65,84 +59,7 @@
                                     <a href="/register.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ext-white md:over:text-blue-500 over:bg-gray-700 over:text-blue-500 md:over:bg-transparent order-gray-700">Register</a>
                                 </li>
                             <?php } ?>
-                                <!--TODO Probably this can be deleted 
-                                <li class="<?php echo (isset($in_checkout)) ? "hidden" : ""; ?>">
-                                    <button class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ext-white md:over:text-blue-500 over:bg-gray-700 over:text-blue-500 md:over:bg-transparent order-gray-700" onclick="toggle_cart()">
-                                        <div class="flex justify-center items-center">
-                                            <div class="relative">
-                                                <div class="t-0 absolute left-3">
-                                                    <p class="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white" id="cart_size">
-                                                        0
-                                                    </p>
-                                                </div>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    </button>
-                                </li>
-                                -->
                         </ul>
                     </div>
                 </div>
-                <!--TODO Probably this can be deleted 
-                <div class="relative z-10 hidden" id="cart_menu" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
-                    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-
-                    <div class="fixed inset-0 overflow-hidden">
-                        <div class="absolute inset-0 overflow-hidden">
-                            <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-                                <div class="pointer-events-auto w-screen max-w-md">
-                                    <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                                        <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                                            <div class="flex items-start justify-between">
-                                                <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">
-                                                    Shopping cart
-                                                </h2>
-                                                <div class="ml-3 flex h-7 items-center">
-                                                    <button type="button" class="relative -m-2 p-2 text-gray-400 hover:text-gray-500" onclick="toggle_cart()">
-                                                        <span class="absolute -inset-0.5" />
-                                                        <span class="sr-only">Close panel</span>
-                                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                                        </svg>
-                                                    </button>
-                                                </div>
-                                            </div>
-
-                                            <div class="mt-8">
-                                                <div class="flow-root">
-                                                    <ul role="list" class="-my-6 divide-y divide-gray-200" id="cart_list">
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
-                                            <div class="flex justify-between text-base font-medium text-gray-900">
-                                                <p>Subtotal</p>
-                                                <p id="cart_total"></p>
-                                            </div>
-                                            <p class="mt-0.5 text-sm text-gray-500">
-                                                Shipping and taxes calculated at checkout.
-                                            </p>
-                                            <div class="mt-6">
-                                                <button onclick="redirectTo()" class="rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm w-full hover:bg-blue-700 disabled:cursor-not-allowed disabled:hover:none
-                                        disabled:opacity-10" id="checkout_button">Checkout</button>
-                                        <script>
-                                            var to_redirect = "<?php echo ($user === null ? "/login.php" : "/checkout.php") ?>"
-                                            const redirectTo = () => {
-                                                window.location.href = to_redirect
-                                            }
-                                        </script>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                -->
             </nav>
